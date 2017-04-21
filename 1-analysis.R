03a6cb39395e604eb23a4de6961f810ab54263cc
svmiller <steven.v.miller@gmail.com>
2017-04-21 13:20:03 -0400

    update script for methods kids

--- 1-analysis.R
+++ 1-analysis.R
@@ -1,18 +1,34 @@
-Data <- read.csv("2016-trump-shift.csv")
+library(RCurl)
+library(stargazer)
 
-M0 <- lm(I((obama/total2012)- (clinton/total2016)) ~ paper, data=Data)
-
-M1 <- lm(I((obama/total2012)- (clinton/total2016)) ~ paper + white + black + 
-           collegeed + log(pop), data=Data)
-
-M2 <- lm(I(total2016 - total2012) ~ paper + white + black + collegeed + log(pop), data=Data)
+data <- getURL("https://raw.githubusercontent.com/svmiller/2016-trump-shift/master/2016-trump-shift.csv")
+Data <- read.csv(text = data)
+summary(Data)
 
 
-stargazer(M0, M1, M2, type="html", title="Simple Regression of Wisconsin County Presidential Election Results",
+Data$voteshare_oc <- with(Data, (obama/total2012)- (clinton/total2016))
+Data$turnoutdiff <- with(Data, total2016 - total2012)
+
+scale2sd <- function(x) {
+  return ((x-mean(x))/(2*sd(x)))
+}
+
+Data$z_logpop <- with(Data, scale2sd(log(pop+1)))
+Data$z_white <- with(Data, scale2sd(log(white+1)))
+Data$z_black <- with(Data, scale2sd(log(black+1)))
+Data$z_collegeed <- with(Data, scale2sd(log(collegeed+1)))
+
+summary(M0 <- lm(voteshare_oc ~ paper, data=Data))
+summary(M1 <- lm(voteshare_oc ~ paper + z_white + z_black + z_collegeed + z_logpop, data=Data))
+summary(M2 <- lm(turnoutdiff ~ paper + z_white + z_black + z_collegeed + z_logpop, data=Data))
+
+
+stargazer(M0, M1, M2, type="text", title="Simple Regression of Wisconsin County Presidential Election Results",
           dep.var.labels=c("Vote Share: Obama - Clinton", "Turnout: 2016 - 2012"),
-          covariate.labels = c("Paper Ballots", "% White", "% Black", "% College Educated",
-                               "County Population (logged)"),
+          covariate.labels = c("Paper Ballots", 
+                               "% White (standardized)", 
+                               "% Black  (standardized)", 
+                               "% College Educated  (standardized)",
+                               "County Population Size (logged, standardized)"),
           omit.stat=c("f","ser"))
 
-top5black <- sqldf("select county, black, trump, clinton, total2016, 
-                   romney, obama, total2012 from Data where black >= 4")
